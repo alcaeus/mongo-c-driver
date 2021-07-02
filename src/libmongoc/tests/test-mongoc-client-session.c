a136a8a6c27ed00640c8365f4f410be94c39f8d8
Andreas Braun <git@alcaeus.org>
2021-07-02 15:16:16 +0200

    Use different database for raw query tests

--- src/libmongoc/tests/test-mongoc-client-session.c
+++ src/libmongoc/tests/test-mongoc-client-session.c
@@ -724,7 +724,7 @@ _test_end_sessions (bool pooled)
    r = mongoc_client_session_append (cs1, &opts1, &error);
    ASSERT_OR_PRINT (r, error);
    r = mongoc_client_command_with_opts (
-      client, "admin", tmp_bson ("{'count': 'c'}"), NULL, &opts1, NULL, &error);
+      client, "test", tmp_bson ("{'count': 'c'}"), NULL, &opts1, NULL, &error);
    ASSERT_OR_PRINT (r, error);
 
    cs2 = mongoc_client_start_session (client, NULL, &error);
@@ -733,7 +733,7 @@ _test_end_sessions (bool pooled)
    r = mongoc_client_session_append (cs2, &opts2, &error);
    ASSERT_OR_PRINT (r, error);
    r = mongoc_client_command_with_opts (
-      client, "admin", tmp_bson ("{'count': 'c'}"), NULL, &opts2, NULL, &error);
+      client, "test", tmp_bson ("{'count': 'c'}"), NULL, &opts2, NULL, &error);
    ASSERT_OR_PRINT (r, error);
 
    /*
